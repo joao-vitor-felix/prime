f154b641a3399a3d986bc645256ba6c462275fcf
joao-vitor-felix <joaovitorfelixcontato@gmail.com>
2023-10-19 15:40:42 -0300

    feat: added next auth route and provider

---
+++ src/app/api/auth/authOptions.ts
@@ -0,0 +1,15 @@
+import { NextAuthOptions } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { prisma } from "../../../../prisma/client";
+
+export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET
+    })
+  ],
+  secret: process.env.NEXTAUTH_SECRET
+};
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { PropsWithChildren } from "react";
 import Navigation from "@/components/ui/Navigation/Navigation";
+import { AuthProvider } from "@/providers/auth";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,8 +16,10 @@ export default function RootLayout({ children }: PropsWithChildren) {
   return (
     <html lang="pt-BR">
       <body className={inter.className}>
-        <Navigation />
-        {children}
+        <AuthProvider>
+          <Navigation />
+          {children}
+        </AuthProvider>
       </body>
     </html>
   );
