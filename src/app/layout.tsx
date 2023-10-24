b3e6a656cecac073d3900062154983db67849488
joao-vitor-felix <joaovitorfelixcontato@gmail.com>
2023-10-24 18:31:18 -0300

    style: change font to manrope

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,21 @@
 import "./globals.css";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Manrope } from "next/font/google";
 import { PropsWithChildren } from "react";
-import Navigation from "@/components/ui/Navigation/Navigation";
 import { AuthProvider } from "@/providers/auth";
+import Navigation from "@/components/ui/Navigation/Navigation";
 
-const inter = Inter({ subsets: ["latin"] });
+const manrope = Manrope({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Prime",
-  description: "Sua loja online de alto nível"
+  description: "Sua loja online de alto nível."
 };
 
 export default function RootLayout({ children }: PropsWithChildren) {
   return (
     <html lang="pt-BR">
-      <body className={inter.className}>
+      <body className={manrope.className}>
         <AuthProvider>
           <Navigation />
           {children}
