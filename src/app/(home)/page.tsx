d4cf2e60474cf81f27d98bda3dc2bde75acb609d
joao-vitor-felix <joaovitorfelixcontato@gmail.com>
2023-10-24 18:30:49 -0300

    feat: add first discount banner to Home

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="p-5">
+      <Image
+        src="/banner-home-01.svg"
+        alt="Produtos com até 55% de desconto neste mês"
+        width={0}
+        height={0}
+        className="h-auto w-full"
+        sizes="100wv"
+      />
+    </main>
+  );
+}
