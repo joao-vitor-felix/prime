95366525b5ffe40bc8b6517872b4fac7f15c2786
joao-vitor-felix <joaovitorfelixcontato@gmail.com>
2023-11-01 15:09:11 -0300

    refactor: change keys from name to slug on categoryIcon function inside CategoryItem

--- src/app/(home)/components/CategoryItem.tsx
+++ src/app/(home)/components/CategoryItem.tsx
@@ -8,6 +8,7 @@ import {
   SpeakerIcon,
   SquareIcon
 } from "lucide-react";
+import Link from "next/link";
 
 type CategoryItemProps = {
   category: Category;
@@ -15,21 +16,23 @@ type CategoryItemProps = {
 
 const CategoryItem: React.FC<CategoryItemProps> = ({ category }) => {
   const categoryIcon = {
-    Mouses: <MouseIcon size={16} />,
-    Teclados: <KeyboardIcon size={16} />,
-    Fones: <HeadphonesIcon size={16} />,
-    Mousepads: <SquareIcon size={16} />,
-    Monitores: <MonitorIcon size={16} />,
-    Speakers: <SpeakerIcon size={16} />
+    mouses: <MouseIcon size={16} />,
+    keyboards: <KeyboardIcon size={16} />,
+    headphones: <HeadphonesIcon size={16} />,
+    mousepads: <SquareIcon size={16} />,
+    monitors: <MonitorIcon size={16} />,
+    speakers: <SpeakerIcon size={16} />
   };
   return (
-    <Badge
-      variant="outline"
-      className="flex cursor-pointer items-center justify-center gap-2 rounded-lg py-3"
-    >
-      {categoryIcon[category.name as keyof typeof categoryIcon]}
-      <span className="text-xs font-bold">{category.name}</span>
-    </Badge>
+    <Link href={`/product/${category.slug}`}>
+      <Badge
+        variant="outline"
+        className="flex cursor-pointer items-center justify-center gap-2 rounded-lg py-3"
+      >
+        {categoryIcon[category.slug as keyof typeof categoryIcon]}
+        <span className="text-xs font-bold">{category.name}</span>
+      </Badge>
+    </Link>
   );
 };
 
