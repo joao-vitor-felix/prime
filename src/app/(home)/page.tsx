35a4f84fda0e34117d7cf0964b3c307547524cb3
joao-vitor-felix <joaovitorfelixcontato@gmail.com>
2023-11-03 11:43:16 -0300

    feat: add mouses and keyboards to Home

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -3,6 +3,8 @@ import { prisma } from "../../../prisma/client";
 import ProductList from "@/components/ui/ProductList";
 import SectionTitle from "@/components/ui/SectionTitle";
 import PromoBanner from "./components/PromoBanner";
+import Footer from "@/components/ui/Footer";
+import Link from "next/link";
 
 export default async function Home() {
   const deals = await prisma.product.findMany({
@@ -12,25 +14,69 @@ export default async function Home() {
       }
     }
   });
+
+  const keyboards = await prisma.product.findMany({
+    where: {
+      category: {
+        slug: "keyboards"
+      }
+    }
+  });
+
+  const mouses = await prisma.product.findMany({
+    where: {
+      category: {
+        slug: "mouses"
+      }
+    }
+  });
+
   return (
-    <main className="p-5">
-      <PromoBanner
-        src="/banner-home-01.svg"
-        alt="Produtos com até 55% de desconto neste mês"
-      />
+    <main>
+      <div className="p-5">
+        <Link href="/deals">
+          <PromoBanner
+            src="/banner-home-01.svg"
+            alt="Produtos com até 55% de desconto neste mês"
+          />
+        </Link>
+        <Categories />
 
-      <Categories />
+        <section className="mt-6">
+          <Link href="/deals">
+            <SectionTitle>OFERTAS</SectionTitle>
+          </Link>
+          <ProductList products={deals} />
+        </section>
 
-      <section className="mt-6">
-        <SectionTitle>OFERTAS</SectionTitle>
-        <ProductList products={deals} />
-      </section>
+        <PromoBanner
+          className="mt-6"
+          src="/banner-home-02.svg"
+          alt="Produtos com até 55% de desconto neste mês"
+        />
 
-      <PromoBanner
-        className="mt-6"
-        src="/banner-home-02.svg"
-        alt="Produtos com até 55% de desconto neste mês"
-      />
+        <section className="mt-6">
+          <Link href="/category/keyboards">
+            <SectionTitle>TECLADOS</SectionTitle>
+          </Link>
+          <ProductList products={keyboards} />
+        </section>
+
+        <PromoBanner
+          className="mt-6"
+          src="/banner-home-03.svg"
+          alt="Até 20% de desconto em fones"
+        />
+
+        <section className="mt-6">
+          <Link href="/category/mouses">
+            <SectionTitle>MOUSES</SectionTitle>
+          </Link>
+          <ProductList products={mouses} />
+        </section>
+      </div>
+
+      <Footer />
     </main>
   );
 }
