7517296995a00ad49a6fc72d3a13df993586c983
joao-vitor-felix <joaovitorfelixcontato@gmail.com>
2023-11-03 11:39:59 -0300

    feat: add Footer

---
+++ src/components/ui/Footer.tsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <footer className="mt-6 bg-accent px-8 py-4 text-sm">
+      <div className="mx-auto">
+        © 2023 Copyright <span className="font-bold text-primary">Prime</span>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
