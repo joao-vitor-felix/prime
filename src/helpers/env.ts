3fc9b35e3f7c1029f7f6e8eba2ece817fa27d41f
joao-vitor-felix <joaovitorfelixcontato@gmail.com>
2024-02-14 16:22:37 -0300

    feat: add env object to concentrate all env variables

---
+++ src/helpers/env.ts
@@ -0,0 +1,6 @@
+export const env = {
+  DATABASE_URL: process.env.DATABASE_URL,
+  DIRECT_URL: process.env.DIRECT_URL,
+  GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+  GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET
+};
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -3,14 +3,15 @@ import type { AuthOptions } from "next-auth";
 import type { Adapter } from "next-auth/adapters";
 import GoogleProvider from "next-auth/providers/google";
 
+import { env } from "@/helpers/env";
 import { db } from "@/lib/prisma";
 
 export const authOptions: AuthOptions = {
   adapter: PrismaAdapter(db) as Adapter,
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET
+      clientId: env.GOOGLE_CLIENT_ID,
+      clientSecret: env.GOOGLE_CLIENT_SECRET
     })
   ]
 };
