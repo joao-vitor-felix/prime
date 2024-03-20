6dfa7a2047650bb5ab54a22982cbb1b371d6c7c0
João Vitor <joaovitorfelixcontato@gmail.com>
2024-03-20 17:58:20 -0300

    refactor: remove margin top from Footer

--- src/layout/Footer/Footer.tsx
+++ src/layout/Footer/Footer.tsx
@@ -1,7 +1,7 @@
 export const Footer = () => {
   const currentYear = new Date().getFullYear();
   return (
-    <footer className="mt-5 bg-accent px-8 py-5 text-sm">
+    <footer className="bg-accent px-8 py-5 text-xs">
       © {currentYear} Copyright{" "}
       <span className="font-bold text-primary">Prime</span>
     </footer>
