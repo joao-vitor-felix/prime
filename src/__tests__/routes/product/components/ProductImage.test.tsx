b1fe773a69c1482e2c8bd0eb1c513c5aecfc81a5
João Vitor <joaovitorfelixcontato@gmail.com>
2024-05-02 17:40:49 -0300

    test: should render the product image according to current image selected

--- src/__tests__/routes/product/components/ProductImage.test.tsx
+++ src/__tests__/routes/product/components/ProductImage.test.tsx
@@ -1,4 +1,5 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
 import { ProductImage } from "@/app/(routes)/product/[slug]/components/ProductImage";
 
@@ -11,7 +12,7 @@ const renderComponent = () => {
   const product: Product = {
     name: "Product 1",
     images: [
-      " https://image1.jpg",
+      "https://image1.jpg",
       "https://image2.jpg",
       "https://image3.jpg",
       "https://image4.jpg"
@@ -19,10 +20,10 @@ const renderComponent = () => {
   };
   render(<ProductImage name={product.name} images={product.images} />);
 
-  const image = screen.getByAltText(`Imagem do produto ${product.name}`);
+  const image = screen.getByAltText(`Imagem 1 do produto ${product.name}`);
   const buttons = screen.getAllByRole("button");
 
-  return { image, buttons };
+  return { image, buttons, product };
 };
 
 describe("ProductImage", () => {
@@ -32,10 +33,19 @@ describe("ProductImage", () => {
     const [firstImage, secondImage, thirdImage, fourthImage] = buttons;
 
     expect(image).toBeInTheDocument();
-    expect(buttons).toHaveLength(4);
     expect(firstImage).toBeInTheDocument();
     expect(secondImage).toBeInTheDocument();
     expect(thirdImage).toBeInTheDocument();
     expect(fourthImage).toBeInTheDocument();
+  });
+
+  it("should render the product image according to current image selected", async () => {
+    const { image, buttons, product } = renderComponent();
+
+    const secondImage = buttons[1];
+
+    expect(image).toHaveAttribute("alt", `Imagem 1 do produto ${product.name}`);
+    await userEvent.click(secondImage);
+    expect(image).toHaveAttribute("alt", `Imagem 2 do produto ${product.name}`);
   });
 });
