aa63c216ca26468117f8b320d914dfc89245e5a4
João Vitor <joaovitorfelixcontato@gmail.com>
2024-06-06 23:05:49 -0300

    style: add desktop style to Title

--- src/components/typography/Title.tsx
+++ src/components/typography/Title.tsx
@@ -4,7 +4,7 @@ import { cn } from "@/lib/utils";
 
 export const Title = ({ className, ...props }: ComponentProps<"h3">) => {
   return (
-    <h3 className={cn("font-bold", className)} {...props}>
+    <h3 className={cn("font-bold lg:text-lg", className)} {...props}>
       {props.children}
     </h3>
   );
