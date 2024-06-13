07b9f7a575d6bf07ada6ee0d85fedf73af49d48e
João Vitor <joaovitorfelixcontato@gmail.com>
2024-06-13 19:28:52 -0300

    feat: add message when there's no orders found on Orders

--- src/app/(routes)/orders/page.tsx
+++ src/app/(routes)/orders/page.tsx
@@ -1,9 +1,10 @@
 import { ShoppingBasket } from "lucide-react";
 import { Metadata } from "next";
+import Link from "next/link";
 
 import { getOrders } from "@/actions/order/getOrders";
 import { TitleBadged } from "@/components/typography/TitleBadged";
-import { Container } from "@/components/ui";
+import { Button, Container } from "@/components/ui";
 import { auth } from "@/lib/auth";
 
 import { OrderItem } from "./components/OrderItem";
@@ -22,11 +23,25 @@ const Orders = async () => {
 
   const orders = await getOrders(session?.user.id);
 
+  const noOrdersFound = orders.length === 0;
+
   return (
     <Container>
       <TitleBadged icon={<ShoppingBasket size={18} className="text-primary" />}>
         Pedidos
       </TitleBadged>
+      {noOrdersFound && (
+        <p className="text-sm lg:text-2xl">
+          Não há pedidos no momento.{" "}
+          <Button
+            asChild
+            variant="link"
+            className="inline-flex p-0 text-sm lg:text-2xl"
+          >
+            <Link href="/deals">Fazer pedido agora!</Link>
+          </Button>
+        </p>
+      )}
       {orders
         .toSorted(
           (a, b) =>
