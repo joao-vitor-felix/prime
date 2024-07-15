0f6c6054c2e167eb8474c0ba3028d50a55e9242c
João Vitor <joaovitorfelixcontato@gmail.com>
2024-07-15 20:16:02 -0300

    style: change CategoriesButtonItem font size on desktop

--- src/app/(routes)/(home)/components/CategoriesButtonItem.tsx
+++ src/app/(routes)/(home)/components/CategoriesButtonItem.tsx
@@ -7,6 +7,7 @@ type CategoriesButtonItemProps = {
   name: string;
   slug: string;
 };
+
 export const CategoriesButtonItem = ({
   name,
   slug
@@ -15,7 +16,7 @@ export const CategoriesButtonItem = ({
     <Link href={`/category/${slug}`}>
       <Badge
         variant="outline"
-        className="flex justify-center gap-2 rounded-xl py-3 font-bold"
+        className="flex justify-center gap-2 rounded-xl py-3 font-bold lg:text-base"
       >
         {CATEGORY_ICON[slug as keyof typeof CATEGORY_ICON]}
         {name}
