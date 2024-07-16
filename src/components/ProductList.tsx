5da50c03ac70cdb6cb0ec7540efd3ea65a681f1a
João Vitor <joaovitorfelixcontato@gmail.com>
2024-07-15 21:18:02 -0300

    refactor: limit products shown at ProductList

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -9,7 +9,7 @@ type ProductListProps = {
 export const ProductList = ({ products }: ProductListProps) => {
   return (
     <div className="flex gap-4 overflow-x-auto [&::-webkit-scrollbar]:hidden">
-      {products.map(product => (
+      {products.slice(0, 6).map(product => (
         <ProductItem key={product.id} product={product} />
       ))}
     </div>
